4da8b8f8f74ba1a1d04e581220650e2a3b7e531f
Gabriel Dias Santiago <112146772+gabrieldiassantiago@users.noreply.github.com>
2025-08-11 23:44:32 -0300

    corrigindo codigo de verifcar triangulo

--- triangulo.c
+++ triangulo.c
@@ -1,26 +1,29 @@
 #include <stdio.h>
 
+
 void valida_triangulo(int a, int b, int c) {
+    if (a < b+c && b < a+c && c<a+b) {
+        if (a==b  && b == c) {
+        printf("O triangulo e equilatero");
+        } else if ( a== b || a == c || b==c) {
+            printf("O triangulo é isosceles");
+        } else {
+        printf("O triangulo e escaleno");
+        }
+    } else {
     
-    //3 lados iguais equilatero
-    
-    if (a == b && a == c) {
-        printf("O triangulo é equilatero");
-    } else if (a == b || b == c || c == a) {
-        printf("O triangulo é isosceles");
-    } //2 lados iguais = isosceles
-    else {
-        printf("O triangulo é escaleno");
+    printf("Os lados nao formam um triangulo");
+
     }
 }
 
-
 int main()
 {
-     
-    int a, b, c;
-    scanf("%d %d %d", &a, &b, &c);
-    valida_triangulo(a, b, c);
-    
  
+  int a, b, c;
+  scanf("%d %d %d", &a, &b, &c);
+      valida_triangulo(a, b, c);
+
+    
+
 }
