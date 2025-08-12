6d1a16f78f8ac4efa872ca0fd52a0b435ba7c014
Gabriel Dias Santiago <112146772+gabrieldiassantiago@users.noreply.github.com>
2025-08-12 20:49:52 -0300

    Create biblioteca.c

---
+++ codigo/biblioteca.c
@@ -0,0 +1,14 @@
+#include "biblioteca.h"
+#include <stdio.h>
+
+int subtracao(int a, int b) {
+	LOG_DEBUG("Executando a subtração");
+	return a-b;
+}
+int soma(int a, int b) {
+	LOG_DEBUG("Executando a soma");
+	return a+b;
+}
+
+
+
