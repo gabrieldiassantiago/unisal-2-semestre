edd78cd2fee4e04b400a5ad2c7418de4e4bd7adf
Gabriel Dias Santiago <112146772+gabrieldiassantiago@users.noreply.github.com>
2025-08-19 21:16:15 -0300

    Create main.c

---
+++ somatorio/main.c
@@ -0,0 +1,12 @@
+#include "biblioteca.h"
+
+#include <stdio.h>
+
+void main() {
+	
+    int numero;
+    scanf("%d", &numero);
+    
+    int resultado = somatorio(numero);
+	printf("%d", resultado);
+}
