c8b03b4a0713b6833b072bc6a484285a30d36c47
Gabriel Dias Santiago <112146772+gabrieldiassantiago@users.noreply.github.com>
2025-09-05 16:16:30 -0300

    Create exercicio7.c

---
+++ matrizes/exercicio7.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void) {
+
+  int matriz[10][10];
+
+  for (int i = 0; i < 10; i++) {
+    for (int  j =0; j < 10; j++) {
+      if (i<j) {
+        matriz[i][j] = (2 * i) + (7 * j) -2;
+      } else if (i==j) {
+        matriz[i][j] = (3 * (i * i)) -1;
+      } else {
+      matriz[i][j] = (4 * (i * i * i)) - (5 * (j * j)) + 1;
+      }
+    }
+  }
+
+  for (int i = 0; i < 10; i++) {
+      for (int j = 0; j < 10; j++) {
+          printf("%6d ", matriz[i][j]);
+      }
+      printf("\n");
+  }
+  
+
+}
