c100986d814ba66b4d0118e262146f57c4ea529e
Gabriel Dias Santiago <112146772+gabrieldiassantiago@users.noreply.github.com>
2025-09-15 17:21:41 -0300

    refatorando o codigo do exercicio
    tornando as variaveis mais claras

--- vetores/exercicio12.c
+++ vetores/exercicio12.c
@@ -50,5 +50,61 @@ int main() {
     
   }
 
+  /*
+    CODIGO REFATORADO:
+
+    
+#include <stdio.h>
+
+
+int main() {
+    
+  int mesas[30];
+    int quantidade_lugares_desejados;
+    int codigo_mesa;
+    int limite_ocupado = 0;
+
+    for (int i = 0; i < 30; i++) {
+        mesas[i] = 5;
+    }
+
+    while(limite_ocupado < 150) {
+        
+    printf("Qual mesa você quer? (100-129");
+        scanf("%d", &codigo_mesa);
+
+        if (codigo_mesa == 0) {
+            break;
+        }
+
+        if (codigo_mesa < 100 || codigo_mesa > 129) {
+            printf("Tente novamente, é invalido");
+            continue;
+        }
+
+        printf("Quantos lugares você quer para a mesa %d:", codigo_mesa);
+        scanf("%d", &quantidade_lugares_desejados);
+
+        int indice = codigo_mesa - 100;
+
+        if (quantidade_lugares_desejados > mesas[indice]) {
+            printf("Não foi possivel realizar sua reserva\n");
+            
+        } else {
+            mesas[indice]-=quantidade_lugares_desejados;
+            limite_ocupado+=quantidade_lugares_desejados;
+            printf("Reserva feita para a mesa %d e lugares %d, agora a mesa tem %d disponiveis\n", codigo_mesa, quantidade_lugares_desejados, mesas[indice]);
+
+        }
+
+        
+        
+    }
+
+    printf("Programa finalizado. Total de lugares reservados: %d\n", limite_ocupado);
+
+}
+    */
+
   
 }
