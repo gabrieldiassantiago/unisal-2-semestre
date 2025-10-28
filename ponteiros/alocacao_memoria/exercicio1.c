8bb15835606facfe4bf26676067d8ac5e1ab1c09
Gabriel Dias Santiago <112146772+gabrieldiassantiago@users.noreply.github.com>
2025-10-27 22:52:19 -0300

    Create exercicio1.c

---
+++ ponteiros/alocacao_memoria/exercicio1.c
@@ -0,0 +1,11 @@
+int main()
+{
+  
+   printf("char: %lu bytes\n", sizeof(char));
+  printf("int: %lu bytes\n", sizeof(int));
+    printf("float: %lu bytes\n", sizeof(float));
+      printf("double: %lu bytes\n", sizeof(double));
+
+  //sizeof(variavel); retorna o numero de bytes que ele ocupa
+
+}
