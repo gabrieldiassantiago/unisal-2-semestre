403b9c0941ca5d7702902eac7456c53fd01b904e
Gabriel Dias Santiago <112146772+gabrieldiassantiago@users.noreply.github.com>
2025-11-15 20:17:29 +0000

    Atualiza a função de cálculo de temperatura para converter de Kelvin para Fahrenheit

--- ponteiros/exercicio_lista8.c
+++ ponteiros/exercicio_lista8.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
 
-void calcular(float *temperatura_kelvin, float *temperatura_celsius) {
+void calcular(float *temperatura_kelvin, float *temperatura_fahrenheit) {
     for (int i = 0; i < 24; i++) {
-       *(temperatura_celsius + i) = *(temperatura_kelvin + i) - 273.15;
+        *(temperatura_fahrenheit + i) = (*(temperatura_kelvin + i) - 273.15) * 9.0/5.0 + 32.0;
     }
 }
 
 int main() {
-    float temperatura_kelvin[24], temperatura_celsius[24];
+    float temperatura_kelvin[24], temperatura_fahrenheit[24];
 
     for (int i = 0; i < 24; i++) {
         printf("Digite a temperatura em Kelvin da hora %d: ", i);
-        scanf("%f", &temperatura_kelvin[i]);
+        scanf("%f", temperatura_kelvin + i);
     }
 
-    calcular(temperatura_kelvin, temperatura_celsius);
+    calcular(temperatura_kelvin, temperatura_fahrenheit);
 
+    printf("\nHora\tKelvin\tFahrenheit\n");
     for (int i = 0; i < 24; i++) {
-        printf("Hora %d: %.2f Celsius\n", i, *(temperatura_celsius + i));
+        printf("%2d\t%.2f\t%.2f\n", i, *(temperatura_kelvin + i), *(temperatura_fahrenheit + i));
     }
+
+    return 0;
 }
