cf22ddbaf1d2c5388fc934f0bb3549fee5e6d78e
Gabriel Dias Santiago <112146772+gabrieldiassantiago@users.noreply.github.com>
2025-11-18 15:24:51 +0000

    subindo pra insuportavel

---
+++ ponteiros/alocacao_memoria/exercicio4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int *criarVetor(int N) {
+    int *vetor = malloc(N * sizeof(int));
+
+    if (vetor == NULL)
+    {
+        printf("Erro na alocação de memória!\n");
+        return -1;
+    }
+
+    for (int i = 0; i < N; i++) {
+        *(vetor + i) = i;
+    }
+
+    return vetor;
+    
+}
+
+int main() {
+    int N;
+
+    scanf("%d", &N);
+    int *vetor = criarVetor(N);
+    for (int i = 0; i < N; i++) {
+        printf("%d ", *(vetor + i));
+    }
+    free(vetor);
+}
